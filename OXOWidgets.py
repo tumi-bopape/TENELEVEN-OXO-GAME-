ca5dd9844361ff92910a499dff2c6b5475562889
tumibopape <tuumishom@gmail.com>
2026-04-26 02:16:43 +0200

    Refactor TicTacToe UI and improve styling

--- OXOWidgets.py
+++ OXOWidgets.py
@@ -1,6 +1,7 @@
 import sys, random
 from PyQt5.QtWidgets import*
 from PyQt5.QtGui import*
+from PyQt5.QtCore import Qt
 
 
 class TicTacToe(QWidget):
@@ -10,50 +11,89 @@ class TicTacToe(QWidget):
         self.setGeometry(250,250,200, 150)
         
         # Creating the game labels 
-        self.title_label = QLabel("ULTIMATE OXO GAME", self)  
-        self.title_label.setFont(QFont("Orbitron", 20, ))
-        self.title_label.setStyleSheet("colour: #3ABEFF;")
+        self.title_label1 = QLabel("ULTIMATE", self)  
+        self.title_label1.setFont(QFont("Orbitron", 28,QFont.Bold ))
+        self.title_label1.setStyleSheet("color: white;")
         
-        self.slogan= QLabel("The Ultimate Tic-Tac-Toe Experience")
+        
+        self.oxo_label = QLabel(
+            "<span style='color:#2323FF;'>----- O </span>"
+            "<span style='color:#FF3131;'>X</span>"
+            "<span style='color:#2323FF;'> O -----</span>"
+        )
+        self.oxo_label.setFont(QFont("Montserrat", 15, QFont.Bold))
+        self.oxo_label.setAlignment(Qt.AlignCenter)        
+        
+                
+        self.slogan= QLabel("The Ultimate Tic-Tac-Toe Experience!")
+        self.slogan.setFont(QFont("Orbitron", 8,QFont.Bold ))
+        self.slogan.setStyleSheet("color: white;")
+                
+        
         self.info=QLabel("Click on a cell to make your move")
         self.info2=QLabel("Example: 192.168.1.10")
         
+        self.status_label = QLabel("Status:",self)
+        self.status_label.setFont(QFont("Orbitron",8,QFont.Bold))
+        self.status_label.setStyleSheet("color:white;")
+                
         self.server_label = QLabel("Enter server:", self)
+        self.server_label.setFont(QFont("Arial", 8 ,QFont.Bold ))
+        self.server_label.setStyleSheet("color: white;")              
     
         
-        self.player = QLabel("PLAYERS")
+        self.player = QLabel("Players")
+        self.player.setFont(QFont("Arial", 8 ,QFont.Bold ))
+        self.player.setStyleSheet("color: white;")        
+        
         self.player1= QLabel("Player 1",self)
         self.player2 = QLabel("Player 2",self)    
         
         self.player_design_labelX = QLabel("", self)
         self.player_design_labelO = QLabel("",self)          
         
-        self.game_messages =  QLabel("GAME MESSSAGES")
+        self.game_messages =  QLabel("Game Messages:")
+        self.game_messages.setFont(QFont("Arial", 8 ,QFont.Bold ))
+        self.game_messages.setStyleSheet("color: white;")              
         
         #Creating the game buttons 
         self.closebutton= QPushButton("EXIT",self) 
         self.closebutton.clicked.connect(self.cancel_clicked)
+        self.closebutton.setStyleSheet("background-color:red; color:white;")
         
         self.newgamebutton= QPushButton("NEW GAME", self)
         self.newgamebutton.clicked.connect(self.new_game_button_clicked)
+        self.newgamebutton.setStyleSheet("background-color:green; color:white;")
         
         self.serverbutton =QPushButton("CONNECT", self)
         self.serverbutton.clicked.connect(self.connect_button_clicked)
+        self.serverbutton.setStyleSheet("background-color:blue; color:white;")
         
         self.restartbutton =QPushButton("RESTART",self)
         self.restartbutton.clicked.connect(self.restart_button_clicked)
+        self.restartbutton.setStyleSheet("background-color:grey; color:white;")
         
         #Creating text spaces 
         self.enterserver =QLineEdit(self)
+        self.enterserver.setStyleSheet("background-color:#002147;")
         self.serveroutput= QLabel("",self)
+        self.serveroutput.setStyleSheet("background-color:#002147;")
+        self.statusbar = QLabel("",self)
+        self.statusbar.setStyleSheet("background-color:#002147;")
        
 
         #Creating the board space 
         cross_grid = QGridLayout()
+        cross_grid.setHorizontalSpacing(5)
+        cross_grid.setVerticalSpacing(5) 
+        
+        
         for i in range(0,3):
             for j in range(0,3):
                 self.button = QPushButton("")
-                self.button.clicked.connect(self.button_clicked)
+                self.button.setFixedSize(80,80)
+                self.button.setStyleSheet("background-color:#002147;")
+                self.button.clicked.connect(self.grid_button_clicked)
                 cross_grid.addWidget(self.button,i,j)
         cross_gridWidget = QWidget()
         cross_gridWidget.setLayout(cross_grid)
@@ -105,7 +145,6 @@ class TicTacToe(QWidget):
         grid_widget3.setPalette(QPalette(QColor('#003153')))
         grid_widget3.setAutoFillBackground(True)
         
-    
         
         #Combined game panel grid 
         vbox = QVBoxLayout()
@@ -116,16 +155,19 @@ class TicTacToe(QWidget):
         vbox_widget.setLayout(vbox)        
         
         #creating the left panel 
-        
         grid4 = QGridLayout()
-        grid4.addWidget(self.title_label,0,1)
-        grid4.addWidget(self.slogan,1,1)
+        grid4.addWidget(self.title_label1,0,1)
+        grid4.addWidget(self.oxo_label,1,1)
+        grid4.addWidget(self.slogan,2,1)
+        grid4.addWidget(self.status_label, 3,0)
+        grid4.addWidget(self.statusbar, 3,1)
         grid_widget4 =QWidget()
         grid_widget4.setLayout(grid4)
         
         vbox2 = QVBoxLayout()
         vbox2.addWidget(grid_widget4)
         vbox2.addWidget(cross_gridWidget)
+        vbox2.addWidget(self.info)
         vbox2_widget = QWidget()
         vbox2_widget.setLayout(vbox2)
         vbox2_widget.setPalette(QPalette(QColor('#003153')))
@@ -177,4 +219,4 @@ if __name__ == "__main__":
     game = TicTacToe()
     game.show()
     sys.exit(app.exec_())
-        
+
