5489b949880144426f1058ed057a150eb52eb5ab
MukonaG <mukona.gwele1@gmail.com>
2026-05-03 16:08:53 +0200

    Refactor OXOGUIClient initialization and layout

--- OXOGUIClient.py
+++ OXOGUIClient.py
@@ -230,7 +230,7 @@ class OXOGUIClient(GameClient, QWidget):
         pass 
     
     def handle_message(self,msg):
-        ef handle_message(self,msg):
+       
         if msg.startswith('new game,'):
             self.shape = msg.split(',')[1] #SPLIT THE STRING AS A LIST AND USE THE INDEX 1
             self.clear_board()
